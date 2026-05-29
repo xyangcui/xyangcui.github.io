58802cb1904cdaa2e8a4a37f6352063ce480ee31
cuixy <xyangcui@163.com>
2026-05-29 03:11:31 +0000

    update

---
+++ src/components/links/journals.ts
@@ -0,0 +1,49 @@
+export const amsjournals = [
+  {
+    title: 'Bulletin of the American Meteorological Society',
+    url: 'https://journals.ametsoc.org/view/journals/bams/bams-overview.xml',
+  },
+  {
+    title: 'Journal of the Atmospheric Sciences',
+    url: 'https://journals.ametsoc.org/view/journals/atsc/atsc-overview.xml',
+  },
+  {
+    title: 'Journal of Climate',
+    url: 'https://journals.ametsoc.org/view/journals/clim/clim-overview.xml',
+  },
+  {
+    title: 'Monthly Weather Review',
+    url: 'https://journals.ametsoc.org/view/journals/mwre/mwre-overview.xml',
+  },
+  {
+    title: 'Weather and Forecasting',
+    url: 'https://journals.ametsoc.org/view/journals/wefo/wefo-overview.xml',
+  },
+]
+
+export const agujournals = [
+  {
+    "title": "Geophysical Research Letters",
+    "url": "https://www.agu.org/journals/gl/",
+  },
+  {
+    "title": "Journal of Geophysical Research: Atmospheres",
+    "url": "https://agupubs.onlinelibrary.wiley.com/journal/21698996",
+  },
+  {
+    "title": "Journal of Advances in Modeling Earth Systems",
+    "url": "https://agupubs.onlinelibrary.wiley.com/journal/19422466",
+  },
+  {
+    "title": "Earth's Future",
+    "url": "https://agupubs.onlinelibrary.wiley.com/journal/23284277",
+  },
+  {
+    "title": "AGU Advances",
+    "url": "https://agupubs.onlinelibrary.wiley.com/journal/2576604x",
+ },
+ {
+    "title": "Reviews of Geophysics",
+    "url": "https://agupubs.onlinelibrary.wiley.com/journal/19449208",
+ },
+]
