b44057ca1aedd4593240fc6b0c152a63ccdaaeff
cuixy <xyangcui@163.com>
2026-05-30 13:35:01 +0000

    change

--- src/site.config.ts
+++ src/site.config.ts
@@ -7,7 +7,7 @@ export const theme: ThemeUserConfig = {
   /** Will be used in index page & copyright declaration */
   author: 'Xiangyang Cui',
   /** Description metadata for your website. Can be used in page metadata. */
-  description: 'Stay hungry, stay foolish',
+  /**description: 'Stay hungry, stay foolish',**/
   /** The default favicon for your site which should be a path to an image in the `public/` directory. */
   favicon: '/favicon/favicon.ico',
   /** The default social card image for your site which should be a path to an image in the `public/` directory. */
