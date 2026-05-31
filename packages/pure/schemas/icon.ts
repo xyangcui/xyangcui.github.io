73609b1ea6c2776d2ec14251727d30b5858fc3a8
cuixy <xyangcui@163.com>
2026-05-31 05:02:15 +0000

    change icon

--- packages/pure/schemas/icon.ts
+++ packages/pure/schemas/icon.ts
@@ -3,6 +3,6 @@ import { z } from 'astro/zod'
 import { Icons, type IconsType } from '../libs/icons'
 
 const icons = Object.keys(Icons) as [IconsType, ...IconsType[]]
-
+console.log('icons:', icons)
 /** String that matches the name of one of Starlight’s built-in icons. */
 export const IconSchema = () => z.enum(icons)
